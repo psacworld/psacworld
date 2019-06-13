bec387425eb131cf3cf9237913ab7e87c5938aff
Abdur-RahmaanJ <arj.python@gmail.com>
2019-06-13 10:01:22 +0400

    add3d config info

--- app.ini.php
+++ app.ini.php
@@ -9,12 +9,10 @@ db_user     = "root"
 db_password = ""
 
 [libs]
-;DEV
 autoload_url = "vendor/autoload.php"
-;PROD
-;autoload_url = "../../vendor/autoload.php"
 
 [mail]
+; for local dev use thunderbird and mercury
 mail_smtp_host = "mail.compilertest.online"
 mail_smtp_port = 587
 mail_smtp_debug = 2
@@ -22,16 +20,3 @@ mail_from = "admin@compilertest.online"
 mail_reply = "admin@compilertest.online"
 mail_password = "pass1234pass1234"
 SMTPAuth = true
-
-
-;   PROD SETTINGS
-; remove semicolon to uncomment
-;db_host = "localhost:3306"
-;db_name     = "compile2_psac"
-;db_user     = "compile2_root"
-;db_password = "root1234root1234"
-
-
-;[libs]
-;autoload_url = ../../vendor/autoload.php
-
