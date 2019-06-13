51d7480429901d9ee942d4a004071603e27c427d
Abdur-Rahmaan Janhangeer <arj.python@gmail.com>
2019-06-13 10:07:45 +0400

    Update app.ini.php

--- app.ini.php
+++ app.ini.php
@@ -13,10 +13,10 @@ autoload_url = "vendor/autoload.php"
 
 [mail]
 ; for local dev use thunderbird and mercury
-mail_smtp_host = "mail.compilertest.online"
+mail_smtp_host = "<toconfigure>"
 mail_smtp_port = 587
 mail_smtp_debug = 2
-mail_from = "admin@compilertest.online"
-mail_reply = "admin@compilertest.online"
-mail_password = "pass1234pass1234"
+mail_from = "<toconfigure>"
+mail_reply = "<toconfigure>"
+mail_password = "<toconfigure>"
 SMTPAuth = true
